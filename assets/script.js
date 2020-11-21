2899f655e280acfe7235a3e216d9a8f6c37c1217
sergioalmazan <sergioalmazan22@gmail.com>
2020-11-21 09:08:34 -0500

    added intial var to js

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    var cityToSearch
+    var cardEl = $("<div>")
+    var cardBodyEl = $("<div>")
+    var h5El = $("<h5>")
+    var imgEl = $("<img>")
+    var pTempEl = $("<p>")
+    var pHumidEl = $("<p>")
+
+    cardEl.attr({
+        class: 'card text-white bg-primary m-3',
+        style: 'max-width: 10rem;'
+    });
+    cardBodyEl.attr("class", "card-body");
+    h5El.attr("class", "card-title");
+    pTempEl.attr("class", "card-text");
+    pHumidEl.attr("class", "card-text");
+
+
+    
+});  
